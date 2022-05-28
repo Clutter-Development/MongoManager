56e41027a8c933e3b9aaabe3f6dc938346ed3e98
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-05-28 17:46:17 +0300

    add tests for misc

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,6 +1,8 @@
 from typing import Any, TypeVar
+
 import pytest
-from mongo_manager import find_in_nested_dict, create_nested_dict, maybe_int
+
+from mongo_manager import create_nested_dict, find_in_nested_dict, maybe_int
 
 T = TypeVar("T")
 
@@ -10,10 +12,12 @@ T = TypeVar("T")
     (
         ({"": {"aa": {"b": {"b": 123}}}}, ".aa.b.b", "this shouldnt be accessed", 123),
         ({"": {"": {"cc": {"qqq": "str"}}}}, ["", "", "cc", "qqq"], "this too", "str"),
-        ({"sad": {"d": 123123}}, "sad.d.d.s..s..a.", "default val", "default val")
-    )
+        ({"sad": {"d": 123123}}, "sad.d.d.s..s..a.", "default val", "default val"),
+    ),
 )
-def test_find_in_nested_dict(*, find_in: dict, path: str | list[str], default: Any, expected_result: Any) -> None:
+def test_find_in_nested_dict(
+    *, find_in: dict, path: str | list[str], default: Any, expected_result: Any
+) -> None:
     assert find_in_nested_dict(find_in, path, default=default) == expected_result
 
 
@@ -22,8 +26,8 @@ def test_find_in_nested_dict(*, find_in: dict, path: str | list[str], default: A
     (
         ("abc.def.ghi", 123, {"abc": {"def": {"ghi": 123}}}),
         ("", 444, 444),
-        (["asd", "asd", "ddf"], "str h", {"asd": {"asd": {"ddf": "str h"}}})
-    )
+        (["asd", "asd", "ddf"], "str h", {"asd": {"asd": {"ddf": "str h"}}}),
+    ),
 )
 def test_create_nested_dict(*, path: str | list[str], value: T, expected_result: dict | T) -> None:
     assert create_nested_dict(path, value) == expected_result
@@ -35,8 +39,8 @@ def test_create_nested_dict(*, path: str | list[str], value: T, expected_result:
         ("a string", "a string"),
         ("123", 123),
         ("0", 0),
-        ("123456789123456789123456789123456789", 123456789123456789123456789123456789)
-    )
+        ("123456789123456789123456789123456789", 123456789123456789123456789123456789),
+    ),
 )
 def test_maybe_int(*, value: T, expected_result: int | T) -> None:
     assert maybe_int(value) == expected_result
