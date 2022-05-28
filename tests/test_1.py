0e881dbfddc659b265235e1b45aaf70a6f0b9c32
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-05-28 17:11:41 +0300

    test pytest

--- tests/test_1.py
+++ tests/test_1.py
@@ -2,9 +2,6 @@ import pytest
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "abc",
-    (1, 2, 3)
-)
+@pytest.mark.parametrize("abc", (1, 2, 3))
 async def test_1(abc):
     assert abc in [1, 2], "Fail"
