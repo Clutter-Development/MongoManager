30b8e27e868b860223f05b179d4ff7317a332f64
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-04 19:35:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="MongoManager",
+    name="mongo_manager",
     author="Clutter Development",
     version="1.0.1",
     license="MIT",
